fa9f0fccb747a16bc1f4979d4ebac08b0f10810f
Bob Saska <git@r35.net>
2022-12-18 01:12:24 -0600

    chore: adding kubernetes

--- gather_schemas.sh
+++ gather_schemas.sh
@@ -9,7 +9,10 @@ echo -e "\033[32m==>\033[0m Gathering Schemas";
 
 find $base_directory/schemas/ \
   -name '*.json' \
+  -exclude schemas/kubernets \
   -exec cp -v {} $base_directory/public/ \;
+
+cp -pr $base_directory/schemas/kubernetes $base_directory/kubernetes;
 
 echo "";
 echo -e "\033[36m===\033[0m Done";
