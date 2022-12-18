ec7cad1b5633687d5296bad873bcc0788f0c774a
Bob Saska <git@r35.net>
2022-12-18 01:29:32 -0600

    chore: adding k8s schemas to gather

--- gather_schemas.sh
+++ gather_schemas.sh
@@ -1,18 +1,24 @@
 #!/usr/bin/env bash
 
+# set -x
 set -euo pipefail
 
 base_directory=$(realpath $(dirname $0))
 
 echo "";
-echo -e "\033[32m==>\033[0m Gathering Schemas";
+echo -e "\033[32m==>\033[0m Gathering Non-Kubernetes Schemas";
 
-find $base_directory/schemas/ \
+find $base_directory/schemas \
+  -type f \
+  -not -path "$base_directory/schemas/kubernetes/*" \
+  -and \
   -name '*.json' \
-  -exclude schemas/kubernets \
   -exec cp -v {} $base_directory/public/ \;
 
-cp -pr $base_directory/schemas/kubernetes $base_directory/kubernetes;
+echo ""
+echo -e "\033[32m==>\033[0m Gathering Kubernetes Schemas";
+mkdir -p $base_directory/public/kubernetes
+cp -pr $base_directory/schemas/kubernetes/v* $base_directory/public/kubernetes;
 
 echo "";
 echo -e "\033[36m===\033[0m Done";
